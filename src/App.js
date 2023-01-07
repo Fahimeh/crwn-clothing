3f0aae85d4aa5a5f11c7cdb22d7219677ae1489e
kharazmi.gis@gmail.com <kharazmi.gis@gmail.com>
2023-01-07 13:53:47 +0330

    add header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useNavigate, useParams, Route, Routes } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
 
 
 const HatsPage = () => (
@@ -17,6 +18,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div >
+      <Header/>
       <Routes>
         <Route path='/' element={< HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
